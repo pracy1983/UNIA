8310c6275c6d4172218e0f11d7232d5c32597dff
pracy1983 <paularacy@gmail.com>
2026-03-15 02:33:11 -0300

    Update App.tsx

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,16 +1,14 @@
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import LoginPage from './pages/LoginPage';
 
-function App() {
-              return (
-                              <Router>
-                                    <Routes>
-                                            <Route path="/login" element={<LoginPage />} />
-                                            <Route path="/" element={<LoginPage />} />
-                                    </Routes>Routes>
-                              </Router>Router>
-                            );
+export default function App() {
+    return (
+          <Router>
+                <Routes>
+                        <Route path="/login" element={<LoginPage />} />
+                        <Route path="/" element={<LoginPage />} />
+                </Routes>Routes>
+          </Router>Router>
+        );
 }
-
-export default App;
 </Router>

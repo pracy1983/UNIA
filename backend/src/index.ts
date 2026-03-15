da83979995af34ce84496a9bd325ccf740a6dd74
pracy1983 <paularacy@gmail.com>
2026-03-15 11:02:45 -0300

    Update index.ts

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -37,7 +37,7 @@ if (process.env.NODE_ENV === 'production') {
     const publicPath = path.join(__dirname, 'dist', 'public');
     app.use(express.static(publicPath));
 
-      app.get('*', (req, res) => {
+      app.get('(.*)', (req, res) => {
         res.sendFile(path.join(publicPath, 'index.html'));
   });
 }

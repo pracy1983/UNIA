d53cd5119ca626d3226afa99c5e2a0e5d88fb189
pracy1983 <paularacy@gmail.com>
2026-03-15 01:46:04 -0300

    Update main.tsx

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -4,8 +4,7 @@ import App from './App'
 import './styles/globals.css'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
-    <React.StrictMode>
-        <App />
-    </React.StrictMode>React.StrictMode>,
-  )
-  </React.StrictMode>
+      <React.StrictMode>
+          <App />
+      </React.StrictMode>,
+    )
